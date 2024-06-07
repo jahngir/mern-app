262a6d1e3f1bb347f03526287696fa0b892c0236
Jahangir Naseer <49036214+jahngir@users.noreply.github.com>
2024-06-07 07:40:21 +0500

    new

--- server/index.js
+++ server/index.js
@@ -1,8 +1,45 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const UserModal = require("./models/user");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
+const dotenv = require("dotenv");
+const UserRouter = require("./routes/user");
+const PostRouter = require("./routes/post");
+
+
+dotenv.config();
 const app = express();
 
+// Why Express JS?
+
+// Express was created to make APIs and web applications with ease,
+// It saves a lot of coding time almost by half and still makes web and
+// mobile applications are efficient.
+// Another reason for using express is that it is written in javascript as javascript is an easy language even if you don't have a previous
+
+// restrictions for API which page to asscess and which not in cors
+app.use(
+  cors({
+    origin: ["https://blogappclient-6rfjlhjsl-jahangir-naseers-projects.vercel.app/"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
 app.use(express.json());
+app.use(cookieParser());
+app.use(express.static('storage'))
+mongoose.connect(process.env.MONGO_DB);
+
+
+//New Auth API
+app.use('/auth',UserRouter)
+
+app.use('/api',PostRouter)
 
 app.listen(process.env.PORT, () => {
-    console.log("Server is running");
-  });
+  console.log("Server is running");
+});
