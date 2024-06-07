fcf9afcb4d109ca18e9f62805952d9b438c72f5a
Jahangir Naseer <49036214+jahngir@users.noreply.github.com>
2024-06-07 07:37:32 +0500

    new

--- server/models/user.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const UserSchema = new mongoose.Schema({
-  name: { type: String, required: true, unique: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-});
-const UserModal = mongoose.model("Users", UserSchema);
-module.exports = UserModal;
