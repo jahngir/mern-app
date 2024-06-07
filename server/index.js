e476595b66bd61ac48ef607f2ac4b09b13cd1bd4
Jahangir Naseer <49036214+jahngir@users.noreply.github.com>
2024-06-07 08:38:50 +0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,13 +1,6 @@
 const express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors");
-// const UserModal = require("./models/user");
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
-const cookieParser = require("cookie-parser");
 const dotenv = require("dotenv");
-// const UserRouter = require("./routes/user");
-// const PostRouter = require("./routes/post");
+
 
 
 
@@ -21,18 +14,11 @@ const app = express();
 // mobile applications are efficient.
 // Another reason for using express is that it is written in javascript as javascript is an easy language even if you don't have a previous
 
-// restrictions for API which page to asscess and which not in cors
 
 app.use(express.json());
-app.use(cookieParser());
-app.use(express.static('storage'))
-mongoose.connect(process.env.MONGO_DB);
 
 
-//New Auth API
-// app.use('/auth',UserRouter)
 
-// app.use('/api',PostRouter)
 
 app.get("/", (req,res)=>{
   res.status(200).send("Server is running")
